5c625fa6d6cdf9311694483bd5ad345fcd13fc4f
QuantumGlitch <soaxpi@gmail.com>
2020-10-02 16:36:08 +0200

    fixed post save

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports =
   // Enable oldValues on docs for this schema
   function plugin(schema) {
     if (schema.oldValues) return;
-
     // Remember this plugin was applied
     schema.oldValues = true;
-    schema.post('init', (doc) => {
+
+    function setOldValues(doc) {
       doc.$locals.old = {};
 
       // copy values
@@ -21,5 +21,8 @@ module.exports =
 
         return currentValue;
       };
-    });
+    }
+
+    schema.post('init', setOldValues);
+    schema.post('save', setOldValues);
   };
